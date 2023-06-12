22c9a20d3643f817e97c32bca241a843a64dd80a
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-06-13 01:33:54 +0530

    Basic Setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const db = require("./config/mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+
+// Accessing Routes
+app.use("/", require("./routes/index"));
 
 app.listen(port, function (err) {
   if (err) {
